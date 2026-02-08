431aa69782dd9cd74ab9d7ae25a6636cc41ca99a
Thanakhon Onklan <68720627+ThanakhonOonklan@users.noreply.github.com>
2026-02-08 13:08:19 +0700

    fix: update import paths to use @ alias and configure tsconfig for build

--- src/screens/MostBorrowedEquipment/MostBorrowedEquipment.tsx
+++ src/screens/MostBorrowedEquipment/MostBorrowedEquipment.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MainLayout } from '../../components/layout';
+import { MainLayout } from '@/components/layout';
 
 export const MostBorrowedEquipment: React.FC = () => {
   return (
